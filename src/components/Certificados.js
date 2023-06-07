370a8a6cc17fdb0777a6db72e5bb84af98c7f7f3
Anderson Londoño <84043589+Anderlonva@users.noreply.github.com>
2023-06-07 10:06:28 -0500

    :zap: tilde educación

--- src/components/Certificados.js
+++ src/components/Certificados.js
@@ -1,54 +1,54 @@
-import React, { useState, useEffect }  from 'react'
+import React, { useState, useEffect } from 'react'
 import { diplomas, certificados } from '../estudios'
 import { RowEstudios } from './RowEstudios'
 import { RowCertificados } from './RowCertificados'
 
 export const Certificados = () => {
 
-    const [ img, setImg ] = useState("")
-    const [ arrDiplomas, setArrDiplomas ] = useState([])
-    const [ arrCertificados, setCertificados ] = useState([])
+    const [img, setImg] = useState("")
+    const [arrDiplomas, setArrDiplomas] = useState([])
+    const [arrCertificados, setCertificados] = useState([])
 
-    useEffect( () => {
+    useEffect(() => {
         setArrDiplomas(diplomas)
         setCertificados(certificados)
-    },[])
-    
-  return (
-    <>
-   <div className='container margin-container'>
-   <div className='row mb-2'>
-        <div className='col-lg-6 col-md-6 col-sm-12 text-center mt-3 border mx-1'>
-            <h2 className='mt-3'>Educacion</h2>
-            
-            {
-                arrDiplomas.map( (diploma) => {
-                    return <RowEstudios key={diploma.id} diploma={diploma} />
-                })
-            }
+    }, [])
 
-        </div>
-        
-        <div className='col-lg-5 col-md-5 col-sm-12 text-center mt-3 border mx-1'>
-            <h2 className='mt-3'>Certificados</h2>
-            
-            {
-                arrCertificados.map( (certificado) => {
-                    return <RowCertificados key={certificado.id} certificado={certificado} />
-                })
-            }
-            
+    return (
+        <>
+            <div className='container margin-container'>
+                <div className='row mb-2'>
+                    <div className='col-lg-6 col-md-6 col-sm-12 text-center mt-3 border mx-1'>
+                        <h2 className='mt-3'>Educación</h2>
 
-        </div>
-    </div>
-    <div className='row'>
-        
-        
-        <div className='col-6'>
-            
-        </div>
-    </div>
-   </div>
-    </>
-  )
+                        {
+                            arrDiplomas.map((diploma) => {
+                                return <RowEstudios key={diploma.id} diploma={diploma} />
+                            })
+                        }
+
+                    </div>
+
+                    <div className='col-lg-5 col-md-5 col-sm-12 text-center mt-3 border mx-1'>
+                        <h2 className='mt-3'>Certificados</h2>
+
+                        {
+                            arrCertificados.map((certificado) => {
+                                return <RowCertificados key={certificado.id} certificado={certificado} />
+                            })
+                        }
+
+
+                    </div>
+                </div>
+                <div className='row'>
+
+
+                    <div className='col-6'>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
